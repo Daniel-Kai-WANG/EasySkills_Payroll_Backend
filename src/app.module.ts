ef9316bde62b6c7c24dd3cfb7c9169498ba72cc2
Kai Wang <wayy15308969733@163.com>
2024-06-18 00:52:01 +1000

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayrollsModule } from './payrolls/payrolls.module';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: '123456789',
-    //   database: 'easyskill',
-    //   entities: [],
-    //   synchronize: true,
-    //   autoLoadEntities: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456789',
+      database: 'easyskill',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     PayrollsModule,
   ],
   controllers: [AppController],
