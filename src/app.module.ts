2224aefcb518983cef0c309bc526df9cc1525759
Kai Wang <wayy15308969733@163.com>
2024-06-25 11:54:23 +1000

    update current process

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
+      username: 'test',
       password: '123456789',
       database: 'easyskill',
       entities: [],
